3661efc4cbf074cce7cbebb08e3497fa34f4ba7b
Arnoud de Boer <a.deboer@geonovum.nl>
2020-07-16 10:25:48 +0200

    initiele versie Respec

---
+++ config.js
@@ -0,0 +1,45 @@
+var respecConfig = {
+  specStatus: "GN-WV",
+  specType: "IM",
+  //subtitle: "BRO gegevenscatalogus",
+   //publishDate: "2020-03-31", 
+  logos: [
+  {src: 'https://docs.geostandaarden.nl/bro/gen/logo_bro.png',
+    href: "http://basisregistratieondergrond.nl",
+    alt: "Basisregistratie Ondergrond",
+    width: 252,
+    height: 130,
+    id: 'BRO'},
+  {
+    src: 'https://docs.geostandaarden.nl/bro/gen/logo_minbzk.png',
+    href: "https://www.geobasisregistraties.nl/basisregistraties/ondergrond",
+    alt: "Ministerie BZK",
+    width: 229,
+    height: 130,
+    id: 'BZK',
+  }]  ,
+  editors: [],
+  authors: [
+    {
+      name: "TNO Geologische Dienst Nederland",
+	  mailto: "support@broservicedesk.nl",
+	  note: "088 – 8664 999",
+      companyURL: "http://www.tno.nl",
+    }],
+  shortName: "wdm",
+  pubDomain: "bro",
+  github: "https://github.com/BROprogramma/WDM",
+  //previousPublishDate: "2017-01-01",
+  //previousStatus: "GN-CV",
+  localBiblio: {
+    
+    },
+  //  "ISO19136": {
+  //    "href": "https://www.iso.org/standard/32554.html",
+  //    "title": "ISO 19136:2007 Geographic information -- Geography Markup Language (GML)",
+  //    "authors": [""],
+  //    "date": "September 2007",
+  //    "publisher": "International Organization for Standardization",
+  //  },
+  //},
+};
