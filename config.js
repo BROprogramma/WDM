3672f8325d2f4e8ef3ab09f3879a228a819d2bc4
Arnoud de Boer <a.deboer@geonovum.nl>
2020-10-01 11:08:15 +0200

    Update config.js

--- config.js
+++ config.js
@@ -33,19 +33,61 @@ var respecConfig = {
   //previousStatus: "GN-CV",
   localBiblio: { 
    
-   "Bouwmans": {
+    "Bouwmans1990": {
       "href": "",
       "title": "Achtergrond en toepassing van de TCGB-tabel: een methode voor het bepalen van de opbrengstdepressie van grasland op zandgrond als gevolg van een grondwaterstandsverlaging",
       "authors": ["J. Bouwmans"],
       "date": "1990",
       "publisher": "Technische Commissie Grondwaterbeheer,Utrecht",
     },
- "Finke2004": {
+ "Finke2002": {
       "href": "",
       "title": "Klimaatsrepresentatieve grondwaterspiegeldiepte in Waterschap Peel en Maasvallei",
-      "authors": ["P.A Finke, M.F.P. Bierkens, D.J. Brus, J.W.J. van der Gaast, T. Hoogland, M. Knotters en F. de Vries"],
-      "date": "2004",
+      "authors": ["P.A Finke", "M.F.P. Bierkens", "D.J. Brus", "J.W.J. van der Gaast", "T. Hoogland", "M. Knotters", "F. de Vries"],
+      "date": "2002",
       "publisher": "Wageningen, Alterra-rapport 383",
-    },
+    },	
+	"Finke2004": {
+      "href": "",
+      "title": "Mapping groundwater dynamics using multiple sources of exhaustive high resolution data",
+      "authors": ["P.A Finke", "D.J. Brus, M.F.P. Bierkens", "T. Hoogland", "M. Knotters", "F. de Vries"],
+      "date": "2004",
+      "publisher": "Geoderma 123: 23-39",
+    },	
+	"Hoogland2014": {
+      "href": "",
+      "title": "Actualisatie van de grondwatertrappenkaart van holoceen Nederland",
+      "authors": ["T. Hoogland", "M. Knotters", "M. Pleijter", "D.J.J. Walvoort"],
+      "date": "2014",
+      "publisher": "Wageningen, Alterra-rapport 2612",
+    },	
+	"Ritzema2012": {
+      "href": "",
+      "title": "Meten en interpreteren van grondwaterstanden. Analyse van methodieken en nauwkeurigheid",
+      "authors": ["H.P. Ritzema", "G.B.M. Heuvelink", "M. Heinen", "P.W. Bogaart", "F.J.E. van der Bolt", "M.J.D. Hack-ten Broeke", "T. Hoogland", "M. Knotters", "H.T.L. Massop", "H.R.J. Vroon"],
+      "date": "2012",
+      "publisher": "Wageningen, Alterra-rapport 2345",
+    },	
+	"Runhaar2014": {
+      "href": "",
+      "title": "Hydrologische Randvoorwaarden Natuur Versie 3; Gebruikershandleiding",
+      "authors": ["H. Runhaar", "S. Hennekens"],
+      "date": "2014",
+      "publisher": "Wageningen, Nieuwegein, Utrecht, Alterra Wageningen UR, KWR Watercycle Research Institute, STOWA",
+    },	
+	"Stuyt2018": {
+      "href": "",
+      "title": "Basisregistratie Ondergrond - Gd-kartering Laag-Nederland 2018; Provincie Flevoland",
+      "authors": ["L.C.P.M. Stuyt", "M. Knotters", "D.J.J. Walvoort", "F. Brouwer", "H.T.L. Massop"],
+      "date": "2018",
+      "publisher": "Wageningen, Wettelijke Onderzoekstaken (WOt) Natuur & Milieu. WOt-technical report 145",
+    },	
+	"WHT1987": {
+      "href": "",
+      "title": "De invloed van de waterhuishouding op de landbouwkundige produktie",
+      "authors": ["Werkgroep HELP-tabel"],
+      "date": "1987",
+      "publisher": "Mededelingen Landinrichtingsdienst 176, Utrecht",
+    },	
   },
 };
